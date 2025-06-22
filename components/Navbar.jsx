8ee8f66e3be0efbf6ed4aff6ff17cb543bea084c
Omkar Kathile <omkarkathile420@gmail.com>
2025-06-22 14:26:13 +0530

    ok

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -20,7 +20,10 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto flex items-center justify-between p-4">
         {/* Logo */}
         <div className="flex items-center space-x-2">
-          
+        
+          <span className="text-4xl font-extrabold tracking-wider font-serif ">
+            Omkar Web
+          </span>
         </div>
 
         {/* Desktop Links */}
